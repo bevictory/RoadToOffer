6f3f1b20777f9c1add02543eee13c0471c23cf8d
bevictory <sdkdguohui@163.com>
2016-04-21 11:54:55 +0800

    break Integer 2

--- BreakInteger.cpp
+++ BreakInteger.cpp
@@ -3,14 +3,27 @@
 using namespace std;
 class Solution {
 public:
+    int integerBreak2(int n){
+        if(n==1) return 1;
+        if(n == 2) return 2;
+        if(n==3) return 3;
+        if(n==4) return 4;
+        int result=1;
+        while(n-3>=2){
+            n-=3;
+            result *=3;
+        }
+        result *=n;
+        return result;
+    }
     int integerBreak(int n) {
         if(n==1) return 1;
         int max =0;
-        for(int i=2;i<n;i++){
+        for(int i=2;i<=n;i++){
             int average = n /i;
             int remain = n - average*i;
             int result =1;
-            result*= pow(average, i-remain);
+            result= pow(average, i-remain);
             result *= pow(average+1,remain);
             if(result > max){
                 max = result ;
@@ -22,5 +35,5 @@ public:
 int main(){
     Solution s ;
 
-    cout<<s.integerBreak(5);
+    cout<<s.integerBreak2(7);
 }
